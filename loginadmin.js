c2b94de9a1702b182307fa9047f784725615b5ad
BeCare666 <becare.fr.ge@gmail.com>
2023-01-24 16:34:40 +0100

    first commit

---
+++ loginadmin.js
@@ -0,0 +1,23 @@
+var usernamep = "dlumiere"
+var passwordp = "dlumiere23++"
+var tableauTotakeid = []
+function connect() {
+    const username = document.getElementById('idusername').value;
+    const password = document.getElementById('idpassword').value;
+    document.getElementById('idusername').value= '';
+        document.getElementById('idpassword').value = ''
+    if (username === usernamep && password === passwordp) {
+        //console.log('kodlsjfnsb,')
+        setTimeout(() => {
+            alert("D-lumière s'est bien connecté")
+            var codesecret = "YESDATA2023"
+            tableauTotakeid.push(codesecret)
+            const last = tableauTotakeid.slice(-1).pop();
+            localStorage.setItem("storageName", last);
+            window.location.replace("admin.html")
+        }, 3000)
+    } else {
+        alert("Mot de passe ou nom d'utilisateur incorrect !")
+    }
+
+}
